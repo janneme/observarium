1f0ae3317589a6f0e9cd61d0453a6566a3f48c55
Jan Nemecek <honza@honznet.net>
2026-07-23 16:39:33 +0200

    Bug fixes

---
+++ client/src/icons/MaxHeightIcon.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let size = '1em'
+</script>
+
+<svg
+  viewBox="0 0 24 24"
+  width={size}
+  height={size}
+  fill="none"
+  stroke="currentColor"
+  stroke-width="1.8"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  {...$$restProps}
+>
+  <line x1="3" y1="18" x2="21" y2="18" />
+  <line x1="5" y1="18" x2="16" y2="5" />
+  <path d="M15 18A10 10 0 0 0 11.5 10.4" />
+</svg>
---
+++ client/src/icons/SetIcon.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let size = '1em'
+</script>
+
+<svg
+  viewBox="0 0 24 24"
+  width={size}
+  height={size}
+  fill="none"
+  stroke="currentColor"
+  stroke-width="1.8"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  {...$$restProps}
+>
+  <line x1="3" y1="18" x2="21" y2="18" />
+  <path d="M8 18a4 4 0 0 1 8 0" />
+  <line x1="12" y1="3" x2="12" y2="11" />
+  <path d="M8.5 7.5 12 11l3.5-3.5" />
+</svg>
